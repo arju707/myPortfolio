7cb6828c7ac5c21dcbff33b94b9f06f1bb233537
Arjun vinod <arjunvinod707@gmail.com>
2025-03-30 23:25:03 +0530

    prnavbar_Responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import profileimage2 from "/profileimage2.jpeg"
 
 const Hero = () => {
   return (
-    <div className='text-white'>Hero</div>
+    <div id='home' className='text-white px16
+     flex min-h-scree w-full items-center py-28 md:px-32'>
+      <div className='flex flex-col items-center gap-10 text-white'>
+    <div>
+      <img src={profileimage2} alt="" />
+    </div>
+      </div>
+     </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
+import { div } from "framer-motion/client";
 import React, { useState } from "react";
+import { BiMenu, BiX } from "react-icons/bi";
 import { BsGithub, BsLinkedin, BsTwitterX, BsYoutube } from "react-icons/bs";
 
 
@@ -7,6 +9,10 @@ const Navbar = () => {
 
     const [isOpen,setIsOpen]=useState(false);
 
+
+    const menuOpen=()=>{
+      setIsOpen(!isOpen);
+    }
 
 
 
@@ -71,6 +77,68 @@ const Navbar = () => {
 
 
 </ul>
+
+
+{isOpen ? (
+  <BiX className=" block md:hidden text-4xl"onClick={menuOpen}/>
+):(
+  <BiMenu  className=" block md:hidden text-4xl"onClick={menuOpen}/>
+)}
+
+{isOpen && (
+  <div className={`fixed right-0 top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-1 border-gray-800 bg-black/90 p-12 ${isOpen ? "block":"hidden"}`}>
+    <ul className="flex flex-col gap-9">
+    <a
+          href="#home"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+            <li>Home</li>
+
+        </a>
+
+        <a
+          href="#Tech"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+            <li>Tech</li>
+            
+        </a>
+
+        <a
+          href="#Projects"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+            <li>Projects</li>
+            
+        </a>
+
+        <a
+          href="#Contact"
+          className="cursor-pointer opacity-70 transition-all duration-300 hover:opacity-100"
+        >
+            <li>Contact</li>
+            
+        </a>
+    </ul>
+    <ul  className="flex flex-wrap gap-5">
+
+
+    <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-blue-500 hover:opacity-100"><BsLinkedin/>  </li>
+
+    <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-orange-500 hover:opacity-100"><BsGithub/>  </li>
+    <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-red-500 hover:opacity-100"><BsYoutube/>  </li>
+    <li className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:text-sky-500 hover:opacity-100"><BsTwitterX/>  </li>
+
+
+    
+
+
+
+</ul>
+  </div>
+)
+
+}
 
     </nav>
   );
